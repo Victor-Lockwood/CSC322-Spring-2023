ee874a88566210a70d07d80187d4cc9b8ffbb152
Victor Lockwood <vlockwoo@oswego.edu>
2023-01-27 17:23:44 -0500

    Interim commit

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+
+struct Room {
+    //Self-explanatory, the ID of the room
+    int id;
+
+    //State values: 0 for clean, 1 for half-dirty, 2 for dirty
+    int state;
+
+    //ID of the room to the north of this one; -1 if there is none
+    int northRoomId;
+
+    //ID of the room to the south of this one; -1 if there is none
+    int southRoomId;
+
+    //ID of the room to the south of this one; -1 if there is none
+    int eastRoomId;
+
+    //ID of the room to the west of this one; -1 if there is none
+    int westRoomId;
+};
 
 int main() {
-    printf("Hello, World!\n");
+    printf("                    | |                                              \n"
+           "  ___ _ __ ___  __ _| |_ _   _ _ __ ___    __ _  __ _ _ __ ___   ___ \n"
+           " / __| '__/ _ \\/ _` | __| | | | '__/ _ \\  / _` |/ _` | '_ ` _ \\ / _ \\\n"
+           "| (__| | |  __/ (_| | |_| |_| | | |  __/ | (_| | (_| | | | | | |  __/\n"
+           " \\___|_|  \\___|\\__,_|\\__|\\__,_|_|  \\___|  \\__, |\\__,_|_| |_| |_|\\___|\n"
+           "                                           __/ |                     \n"
+           "                                          |___/ \n");
+    int numberOfRooms = 0;
+
+    printf("Please enter the number of rooms:\n");
+    scanf("%i", &numberOfRooms);
+
+    int *roomPointerArray[numberOfRooms];
+
+    for(int i = 0; i < numberOfRooms; i++) {
+        //malloc some shit for each struct otherwise it'll clear outside scope(?)
+        //Room myRoom = Room();
+        //stick the pointer in roomPointerArray
+    }
+
     return 0;
 }
