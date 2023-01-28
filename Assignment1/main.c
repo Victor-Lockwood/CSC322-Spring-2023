3f606d46130fda8ba64c08ca336536c51c9bc4db
Victor Lockwood <vlockwoo@oswego.edu>
2023-01-28 12:43:26 -0500

    Initialize rooms function written

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+
+
+// *** STRUCTS ***
+struct Creature {
+    //Self-explanatory, the ID of the creature
+    int id;
+
+    //Type of the creature: 0 = PC, 1 = animal, 2 = NPC
+    int creatureType;
+
+    //ID of the room the creature is currently in
+    int roomId;
+};
 
 struct Room {
     //Self-explanatory, the ID of the room
@@ -19,7 +32,22 @@ struct Room {
 
     //ID of the room to the west of this one; -1 if there is none
     int westRoomId;
+
+    //Array of pointers to the creatures in the room
+    struct Creature *creatures[10];
 };
+
+
+// *** FUNCTION PROTOTYPES ***
+
+struct Room* initializeRooms(int);
+
+// *** GLOBALS ***
+
+//Main metric of the game
+//If this hits 0 or under, player loses
+//If this hits 80 or above, player wins
+int respect = 40;
 
 int main() {
     printf("                    | |                                              \n"
@@ -34,13 +62,49 @@ int main() {
     printf("Please enter the number of rooms:\n");
     scanf("%i", &numberOfRooms);
 
-    int *roomPointerArray[numberOfRooms];
+    //Initialize the rooms and get a pointer to the array of structs back
+    struct Room* rooms = initializeRooms(numberOfRooms);
 
-    for(int i = 0; i < numberOfRooms; i++) {
-        //malloc some shit for each struct otherwise it'll clear outside scope(?)
-        //Room myRoom = Room();
-        //stick the pointer in roomPointerArray
-    }
+    printf("First room state: %i\n", rooms[0].state);
 
+    free(rooms);
     return 0;
 }
+
+//Initialize the rooms and return a pointer to the array.  The array is dynamically
+//allocated and must be freed when no longer in use.
+struct Room *initializeRooms(int numberOfRooms) {
+    struct Room* rooms = malloc(numberOfRooms * sizeof(struct Room));
+
+    for(int i = 0; i < numberOfRooms; i++) {
+        int inputSize = 5;
+        int inputArray[inputSize];
+
+        //Grab the five numbers given in the input.
+        //In order, they are: state, north, south, east, west
+        printf("Enter inputs for room #%i: \n", i);
+        for(int j = 0; j < inputSize; j ++){
+            scanf("%i", &inputArray[j]);
+        }
+
+        struct Room room =
+                { .id = i,
+                        .state = inputArray[0],
+                        .northRoomId = inputArray[1],
+                        .southRoomId = inputArray[2],
+                        .eastRoomId = inputArray[3],
+                        .westRoomId = inputArray[4]
+                };
+
+        //clear out the creature pointer array
+        for(int j = 0; j < 10; j++) {
+            room.creatures[j] = NULL;
+        }
+
+        //stick the struct in the rooms array
+        rooms[i] = room;
+    }
+
+    return rooms;
+}
+
