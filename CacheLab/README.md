9f4405ed4a4d56481d5d4266e131617802b3d73a
Victor-Lockwood <vlockwoo@oswego.edu>
2023-04-11 09:56:21 -0400

    Setup initial values

---
+++ CacheLab/README.md
@@ -0,0 +1,4 @@
+# Cache Lab
+
+## Compiler Info
+If compiling only GCC and not CMake, you will need the `-lm` flag at the end (eg. `gcc -o cachelab main.c -lm`).
--- CacheLab/main.c
+++ CacheLab/main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    // *** MAIN CACHE VALUES ***
+
+    //The number of sets
+    int S = 0;
+
+    //The number of lines per set
+    int E = 0;
+
+    //The block size, in bytes
+    int B = 0;
+
+    //The number of address bits
+    int m = 0;
+
+    //Fill in those values
+    printf("Please enter an integer value for S:\n");
+    scanf("%i", &S);
+
+    printf("\nPlease enter an integer value for E:\n");
+    scanf("%i", &E);
+
+    printf("\nPlease enter an integer value for B:\n");
+    scanf("%i", &B);
+
+    printf("\nPlease enter an integer value for m:\n");
+    scanf("%i", &m);
+
+
+    // *** CALCULATED CACHE VALUES ***
+
+    //The number of set index bits
+    int s = (int) log2((float) S);
+
+    //The number of block offset bits
+    int b = (int) log2((float) B);
+
+    //The number of tag bits
+    int t = m - (s + b);
+
+    printf("\n\ns = %i\n", s);
+    printf("b = %i\n", b);
+    printf("t = %i\n", t);
 }
