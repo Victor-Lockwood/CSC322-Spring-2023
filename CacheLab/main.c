21726d249f99985bc7ba90b5b1154987442e0c31
Victor-Lockwood <vlockwoo@oswego.edu>
2023-04-13 19:29:42 -0400

    Block offset stuff removed

--- CacheLab/main.c
+++ CacheLab/main.c
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#define LINE_SIZE 5
+
+// *** TYPE DEFINITIONS ***
+
+//Valid bit, hit frequency, tag, set number and last cycle line was hit
+typedef unsigned int line[LINE_SIZE];
+
+
 // *** GLOBALS ***
 
 // ** Main Cache Values **
@@ -20,8 +28,10 @@ int B = 0;
 //The number of address bits
 int m = 0;
 
-//TODO: Figure out what these are for
+//Time it takes to determine a hit or a miss
 int hitTime = 0;
+
+//Additional cycles added on a miss
 int missPenalty = 0;
 
 //Either LFU or LRU
@@ -39,6 +49,8 @@ int b = 0;
 //The number of tag bits
 int t = 0;
 
+int totalCycles = 0;
+
 
 // ** FUNCTION PROTOTYPES **
 void initializeGlobals();
@@ -48,11 +60,6 @@ int main() {
     initializeGlobals();
 
     int totalLines = S * E;
-    int dataCells = 4;          //Number of cells for valid bit, frequency, tag and set number
-
-    //Valid bit, hit frequency, tag, set number and block bytes
-    //TODO: Need to figure out when something was last used
-    typedef unsigned int line[dataCells + B];
 
     //Cache is an array of lines
     line cache[totalLines];
@@ -63,6 +70,7 @@ int main() {
             cache[i + j][1] = 0;   //Hit frequency is 0
             cache[i + j][2] = 0;   //Tag
             cache[i + j][3] = i;   //Set numbers
+            cache[i + j][4] = 0;   //Last cycle this was hit at
 
             //Everything else in a line are blocks
         }
@@ -85,13 +93,16 @@ int main() {
             //Referred to here: https://stackoverflow.com/questions/10156409/convert-hex-string-char-to-int
             intInput = (unsigned int) strtol(input, NULL, 16);
 
+            //DON'T ACTUALLY NEED BLOCK OFFSET
+            //TODO: Clear blockoffset references - just need b for calculations
             //Gives us our block offset
             //Will need to add 4 to it when actually indexing to account for the other slots taken
-            int blockOffsetShift = 32 - b;
-            unsigned int blockOffset = intInput << blockOffsetShift;
-            blockOffset = blockOffset >> blockOffsetShift;
+            //int blockOffsetShift = 32 - b;
+            //unsigned int blockOffset = intInput << blockOffsetShift;
+            //blockOffset = blockOffset >> blockOffsetShift;
 
             //Gives us our set id
+            //TODO: Need to use masks instead, ints should also be longs
             int setOffsetShift = 32 - (s + b);
             unsigned int setNumber = intInput << setOffsetShift;
             setNumber = setNumber >> setOffsetShift;
@@ -114,22 +125,24 @@ int main() {
             bool foundLine = false;
             //TODO: Continue with this, but make the optimized version for direct cache that gets the set directly
             //TODO: Only works for direct cache rn
+            totalCycles += hitTime;
+
             for(int i = 0; i < totalLines; i++) {
                 if (cache[i][3] == setNumber) {
-                    if (cache[i][0] == 1 && cache[i][2] == tag && cache[i][dataCells + blockOffset] == intInput) {
+                    if (cache[i][0] == 1 && cache[i][2] == tag) { //Check valid bit and tag
                         // Got a hit!
                         cache[i][1] += 1;
                         printf("%s H\n", input);
                     } else {
+                        totalCycles += missPenalty;
+
                         //Got a miss - need to add to cache
                         printf("%s M\n", input);
 
                         cache[i][0] = 1;
+                        cache[i][1] += 1;
                         cache[i][2] = tag;
-
-                        for(int j = 0; j < B; j++) {
-                            cache[i][dataCells + j] = intInput + j;
-                        }
+                        cache[i][4] = totalCycles; //Lowest number will determine what to evict when LRU
                     }
                 }
             }
@@ -139,7 +152,8 @@ int main() {
     return 0;
 }
 
-//Get user input to fill in the main parameters of the cache
+//Get user input to fill in the main parameters of the cache\
+//TODO: Clear out debug statements
 void initializeGlobals() {
     //Fill in those values
     printf("Please enter an integer value for S:\n");
